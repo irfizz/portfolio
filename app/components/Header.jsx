4232f19fd1e0e5d0a5fa41c29a053c402123ff84
irfizz <irfan.izzat.azman.88@gmail.com>
2025-02-12 09:18:12 +0800

    padding and height

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = ({ isDarkMode }) => {
           Contact Me
           <Image src={assets.right_arrow_white} alt="" className="w-4" />
         </a>
-        <a href="/Resume-Nur_Irfan_Izzat.pdf" download className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:bg-white dark:text-black">
+        <a href="/Resume - Nur Irfan Izzat.pdf" download className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:bg-white dark:text-black">
           My Resume
           <Image src={assets.download_icon} alt="" className="w-4" />
         </a>
